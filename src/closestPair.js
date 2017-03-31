baaef77530f647a61c544602c246fe5289c90e02
Helen Yau <helenyau13@gmail.com>
2017-03-31 13:55:45 -0700

    working on the logic for closestPair

---
+++ src/closestPair.js
@@ -0,0 +1,8 @@
+export default function closestPair(point) {
+  let mid = (point.length/2)
+  let mid2 = (point.length/(2+1))
+  let subArray = point.slice(0, mid)
+  let subArray2 = point.slice(mid2, (point.length-1))
+
+
+}
